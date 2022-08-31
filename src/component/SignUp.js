c563b50355b35f7da3cf521de4d0473cfc6436b6
rlaqja9199 <rlaqja9199@naver.com>
2022-09-01 01:45:31 +0900

    회원가입 서버 연결중

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -14,6 +14,13 @@ const SignUp = () => {
     const [eMailLineColor, setEMailLineColor] = useState('#eee');
     const [mobileNumberLineColor, setMobileNumberLineColor] = useState('#eee');
     const [questionLineColor, setQuestionLineColor] = useState('#eee');
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        passwordCheck: "",
+        eMail: "",
+        mobileNumber: "",
+    })
 
 
 
@@ -106,57 +113,109 @@ const SignUp = () => {
         }
     };
 
+    const onChange = async (e) =>{
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+    const onSubmit = (e)=>{
+        e.preventDefault();
+
+        if(isNaN(formData.mobileNumber)){
+            alert("전화번호는 숫자만 입력해주세요");
+            setFormData({
+                ...formData,
+                mobileNumber: "",
+            })
+        }
+
+        //유효성검사 - 글자수 제한, 유의미한 데이터인지 파악 등
+        //1. id의 길이(4-10자 입력)
+        if(formData.c_id.length<4 || formData.c_id.length>=11) alert("ID가 양식에 적합하지 않습니다.");
+        //2. 비밀번호의 최소길이 4글자 이상
+        else if(formData.c_password.length<4 || formData.c_pwch.length<4) alert("비밀번호는 4자 이상 입력해주세요.");
+        //3. 비밀번호와 비밀번호확인이 일치하는지
+        else if(formData.c_password !== formData.c_pwch) alert("비밀번호가 일치하지 않습니다.");
+        //4. 전화번호 phone은 3글자 / phone2, phone3은 4글자
+        else if(formData.c_phone.length>=4 || formData.c_phone2.length>=5 || formData.c_phone3.length>=5 ) alert("전화번호 양식에 적합하지 않습니다.");
+        //중복아이디 체크
+        // else {
+        //     if (formData.c_id===)
+        // }
+        //input에 값이 있는지 체크하고
+        if(formData.c_id !== "" && formData.c_password !== "" && formData.c_pwch !== "" && formData.c_name !== "" && formData.c_gender !== "" &&
+        formData.c_phone !== "" && formData.c_add !== "" && formData.c_adddetail !=="" && formData.c_email !==""){
+            insertJoin();
+        }
+    }
+    //insertJoin 함수
+    function insertJoin(){
+        axios.post("http://localhost:8000/signup",formData)
+        // axios.post(`${API_URL}/register`,formData)
+        .then(res=>{
+            console.log(res);
+            // navigate('/');              
+        })
+        .catch(e=>{
+            console.log(e);
+        })
+    }
+
 
   return (
-    <div id='signUp'>
-        <ul id='signUpBox'>
-            <li>
-                <h3>SIGN UP</h3>
-            </li>
-            <li>
-                <p style={{opacity:userNameTextOpacity}}>USERNAME</p>
-                <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='USERNAME' />
-                <div className='inputLine' style={{backgroundColor: userNameLineColor}}></div>
-            </li>
-            <li>
-                <p style={{opacity:passwordTextOpacity}}>PASSWORD</p>
-                <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='PASSWORD' />
-                <div className='inputLine' style={{backgroundColor: passwordLineColor}}></div>
-            </li>
-            <li>
-                <p style={{opacity:passwordCheckTextOpacity}}>PASSWORD CHECK</p>
-                <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='PASSWORD CHECK' />
-                <div className='inputLine' style={{backgroundColor: passwordCheckLineColor}}></div>
-            </li>
-            <li>
-                <p style={{opacity:eMailTextOpacity}}>E-MAIL</p>
-                <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='E-MAIL' />
-                <div className='inputLine' style={{backgroundColor: eMailLineColor}}></div>
-            </li>
-            <li>
-                <p style={{opacity:mobileNumberTextOpacity}}>MOBILE NUMBER</p>
-                <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='MOBILE NUMBER' />
-                <div className='inputLine' style={{backgroundColor: mobileNumberLineColor}}></div>
-            </li>
-            <li>
-                <p style={{opacity:questionTextOpacity}}>QUESTION</p>
-                <div id='questionWrap'>
-                    <select name='question' onClick={inputFocus}>
-                        <option value="eSchool">출신 초등학교는?</option>
-                        <option value="nickname">자신의 별명은?</option>
-                        <option value="speciality">자신의 특기는?</option>
-                        <option value="bestFriend">가장 친한 친구의 이름은?</option>
-                        <option value="bestPlace">가장 좋아하는 장소는?</option>
-                    </select>
-                    <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='QUESTION ANSWER'/>
-                </div>
-                <div className='inputLine' style={{backgroundColor:questionLineColor}}></div>
-            </li>
-            <li>
-                <button id='joinBtn'>JOIN BARON</button>
-            </li>
-        </ul>
-    </div>
+    <form id='signUpForm' onSubmit={onSubmit}>
+        <div id='signUp'>
+            <ul id='signUpBox'>
+                <li>
+                    <h3>SIGN UP</h3>
+                </li>
+                <li>
+                    <p style={{opacity:userNameTextOpacity}}>USERNAME</p>
+                    <input onFocus={inputFocus} onChange={textTypeOpacity} placeholder='USERNAME' />
+                    <div className='inputLine' style={{backgroundColor: userNameLineColor}}></div>
+                </li>
+                <li>
+                    <p style={{opacity:passwordTextOpacity}}>PASSWORD</p>
+                    <input type="text" onFocus={inputFocus} onChange={textTypeOpacity} placeholder='PASSWORD' />
+                    <div className='inputLine' style={{backgroundColor: passwordLineColor}}></div>
+                </li>
+                <li>
+                    <p style={{opacity:passwordCheckTextOpacity}}>PASSWORD CHECK</p>
+                    <input type="text" onFocus={inputFocus} onChange={textTypeOpacity} placeholder='PASSWORD CHECK' />
+                    <div className='inputLine' style={{backgroundColor: passwordCheckLineColor}}></div>
+                </li>
+                <li>
+                    <p style={{opacity:eMailTextOpacity}}>E-MAIL</p>
+                    <input type="text" onFocus={inputFocus} onChange={textTypeOpacity} placeholder='E-MAIL' />
+                    <div className='inputLine' style={{backgroundColor: eMailLineColor}}></div>
+                </li>
+                <li>
+                    <p style={{opacity:mobileNumberTextOpacity}}>MOBILE NUMBER</p>
+                    <input type="text" name='mobileNumber' onFocus={inputFocus} onChange={textTypeOpacity} placeholder='MOBILE NUMBER' />
+                    <div className='inputLine' style={{backgroundColor: mobileNumberLineColor}}></div>
+                </li>
+                <li>
+                    <p style={{opacity:questionTextOpacity}}>QUESTION</p>
+                    <div id='questionWrap'>
+                        <select name='question' onClick={inputFocus}>
+                            <option value="eSchool">출신 초등학교는?</option>
+                            <option value="nickname">자신의 별명은?</option>
+                            <option value="speciality">자신의 특기는?</option>
+                            <option value="bestFriend">가장 친한 친구의 이름은?</option>
+                            <option value="bestPlace">가장 좋아하는 장소는?</option>
+                        </select>
+                        <input type="text" onFocus={inputFocus} onChange={textTypeOpacity} placeholder='QUESTION ANSWER'/>
+                    </div>
+                    <div className='inputLine' style={{backgroundColor:questionLineColor}}></div>
+                </li>
+                <li>
+                    <button id='joinBtn' type='submit'>JOIN BARON</button>
+                </li>
+            </ul>
+        </div>
+    </form>
   )
 }
 
